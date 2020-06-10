e57ece0e14b1067fe20ef080d88b22e55877c62e
Potito Gradone <pgradone@gmail.com>
2020-06-10 18:48:31 +0200

    initialize #newthing value of user

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,20 @@
 refreshHTMLlists();
 
-document.querySelector("form").addEventListener("submit", function (evnt) {
+document.querySelector('form').addEventListener('submit', function (evnt) {
   evnt.preventDefault();
-  const newTaskValue = document.querySelector("#newthing").value;
+  const newTaskValue = document.querySelector('#newthing').value;
   const newTask = { done: false, task: newTaskValue };
   addToList(newTaskValue, false);
   todoArray.push(newTask);
-  console.log(todoArray);
   refreshHTMLlists();
+  console.log(document.querySelector('#newthing').value);
+  document.querySelector('#newthing').value = '';
 });
 
 function refreshHTMLlists() {
   // remove all items from the 2 lists
-  document.querySelector("#listtodo").innerHTML = "";
-  document.querySelector("#listdone").innerHTML = "";
+  document.querySelector('#listtodo').innerHTML = '';
+  document.querySelector('#listdone').innerHTML = '';
   // add all data of the Array to the list
   for (const todoData of todoArray) {
     addToList(todoData.task, todoData.done);
@@ -23,27 +24,27 @@ function refreshHTMLlists() {
 function addToList(task, done) {
   // define the PARENT list item
   const myList = done
-    ? document.querySelector("#listdone")
-    : document.querySelector("#listtodo");
+    ? document.querySelector('#listdone')
+    : document.querySelector('#listtodo');
   // create the 'list' item and append to list
-  const liHTML = document.createElement("li");
+  const liHTML = document.createElement('li');
   myList.append(liHTML);
   // create the 'input' item and append it inside the <li>
-  const inputHTML = document.createElement("input");
+  const inputHTML = document.createElement('input');
   liHTML.append(inputHTML);
   // set attribute type='checkbox' to inputHTML
-  inputHTML.type = "checkbox";
+  inputHTML.type = 'checkbox';
   // set 'checked' attribute of inputHTML to 'done' of data
   inputHTML.checked = done;
   // create the 'span' item, set its content and append to <li> also
-  const spanHTML = document.createElement("span");
+  const spanHTML = document.createElement('span');
   spanHTML.textContent = task;
   liHTML.append(spanHTML);
   // set 'line-through' style to the span text (if done), otherwise none
-  spanHTML.style.textDecoration = done ? "line-through" : "";
+  spanHTML.style.textDecoration = done ? 'line-through' : '';
   // Make sure to update the task when the user clicks on the checkbox.
   // add 'click' event listener to input item by calling function
-  inputHTML.addEventListener("click", function () {
+  inputHTML.addEventListener('click', function () {
     // change the data
     // find out which index of the array will be modified
     for (let index = 0; index < todoArray.length; index++) {
